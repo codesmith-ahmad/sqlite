1e4acce1e06cf4de56721699c78589a2644c98c1
The Codesmith <b-9k@outlook.com>
2024-03-11 16:23:16 -0400

    Removed cursors, execute directly via conenction instead. Added sqlite3 configuration; 1/2: Queries now return sqlite3.Rows instead of tuples, 2/2: Added adapters and converters to convert strings to complex datatypes (Datetime and YA<L)

--- environment/environment.py
+++ environment/environment.py
@@ -1,5 +1,6 @@
+import sqlite3
+import datetime
 from configparser import ConfigParser
-
 from prettytable import PrettyTable
 from myutils import type_library as T
 from myutils import file_module as F
@@ -17,11 +18,12 @@ class Environment:
     
     def __init__(self) -> T.Self:
         info("Intializing Environment")
+        configure_sqlite3()
         self.config = self.load_settings()  # Parse config.ini
-        self.menu_dict = self.load_menu()        # Read list of DBs from config
+        self.menu_dict = self.load_menu()   # Read list of DBs from config
         self.models = self.load_models()    # Load models (DTOs)
         self.connection: Connection = None
-        self.cursor: Cursor = None
+        # self.cursor: Cursor = None          Superfluous
         
     def load_settings(self) -> ConfigParser:
         """Parses config.ini and loads it up in Environment"""
@@ -45,7 +47,7 @@ class Environment:
     
     def select_db(self) -> str:
         """Returns filepath of selected database"""
-        db_names = list(self.menu_dict.keys()) + ["\033[0m"] # secretly adding caption to reset ansi styles
+        db_names = list(self.menu_dict.keys()) + ["\033[0m"] # secretly adding caption to reset ansi styles #TODO Use myutils instead
         db_filepaths = list(self.menu_dict.values())         #   |
         print("\n\033[33mSelect database\033[0m:")           #   |
         index = select(                                      #   |
@@ -60,10 +62,14 @@ class Environment:
     
     def connect(self,data_source:str) -> None:
         info(f"Connecting to {data_source}...")
-        self.connection = connect(data_source)
-        self.cursor = self.connection.cursor()
-        sqlite_version = self.cursor.execute("SELECT sqlite_version()").fetchone()[0]
-        info(f"Successfully connected to SQLite version {sqlite_version}")
+        con = connect(
+            database=data_source,
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        con.row_factory = sqlite3.Row # .execute() will now return Rows instead of tuples. Rows work similar to dict
+        self.connection = con
+        # self.cursor = self.connection.cursor() Superfluous
+        info(f"Successfully connected to SQLite version {con.execute("SELECT sqlite_version()").fetchone()[0]}")
         
     def fetch_tables(self) -> Result:
         r = Result()
@@ -94,10 +100,68 @@ class Environment:
             r.error = e
         finally:
             return r
-
-    @classmethod
-    def __str__(cls):
+        
+    def __str__(self):
         """
         String representation of the class.
         """
-        return f"{cls}"
+        return f"{self}"
+
+#TODO Move this to another my file **************************************
+
+#TODO Refer to https://docs.python.org/3/library/sqlite3.html#adapter-and-converter-recipes
+
+def configure_sqlite3(self):
+    sqlite3.register_adapter(datetime.date, adapt_date_iso)
+    sqlite3.register_adapter(datetime.time, adapt_time_iso)
+    sqlite3.register_adapter(datetime.datetime, adapt_datetime)
+    sqlite3.register_adapter(MyFileObject, adapt_file)
+    sqlite3.register_adapter(MyYAMLObject, adapt_yaml)
+    sqlite3.register_converter("date", convert_date)
+    sqlite3.register_converter("time", convert_time)
+    sqlite3.register_converter("datetime", convert_datetime)
+    sqlite3.register_converter("file", convert_file)
+    sqlite3.register_converter("yaml", convert_yaml)
+
+class MyFileObject:
+    pass
+
+class MyYAMLObject:
+    pass
+
+def adapt_date_iso(val):
+    pass
+
+def adapt_time_iso(val):
+    pass
+
+def adapt_datetime(val):
+    pass
+
+def adapt_file(val):
+    pass
+
+def adapt_yaml(val):
+    pass
+
+def convert_date(val):
+    """Convert ISO 8601 date to datetime.date object."""
+    return datetime.date.fromisoformat(val.decode())
+
+def convert_time(val):
+    """Convert 24-hr time to datetime.time object."""
+    iso_time_string = self.convert_to_iso(val) # TODO 2359 --> T23:59:00Z or sum like that
+    return datetime.time.fromisoformat()
+
+def convert_to_iso(val):
+    raise NotImplementedError()
+
+def convert_datetime(val):
+    """Convert ISO 8601 datetime to datetime.datetime object."""
+    return datetime.datetime.fromisoformat(val.decode())
+
+def convert_file(val):
+    pass
+
+def convert_yaml(val):
+    pass
